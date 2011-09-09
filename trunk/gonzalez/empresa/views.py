aec4d4c27a50f422ee1884f68befe2b533d42d3a
lapis.jumi@gmail.com <lapis.jumi@gmail.com@e957e66a-7c46-a533-5bf8-e8838cd3d126>
2011-09-09 17:51:24 +0000

    Agregada aplicacion de clientes con funcion para resizear las imagenes a un tamano pequeno.

--- trunk/gonzalez/empresa/views.py
+++ trunk/gonzalez/empresa/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import render_to_response, HttpResponse
+
+
+def info_empresa(request):
+    return render_to_response("empresa/empresa.html",{},)
--- trunk/gonzalez/settings.py
+++ trunk/gonzalez/settings.py
@@ -117,8 +117,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gonzalez.empresa',
+    'gonzalez.clientes',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
