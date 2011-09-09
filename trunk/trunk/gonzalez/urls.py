57ea20356346112b37e7ef5ca674db752a2b91c2
lapis.jumi@gmail.com <lapis.jumi@gmail.com@e957e66a-7c46-a533-5bf8-e8838cd3d126>
2011-09-09 17:51:24 +0000

    Agregada aplicacion de clientes con funcion para resizear las imagenes a un tamano pequeno.

--- trunk/trunk/gonzalez/urls.py
+++ trunk/trunk/gonzalez/urls.py
@@ -1,19 +1,20 @@
 from django.conf.urls.defaults import patterns, include, url
 from gonzalez.settings import MEDIA_ROOT
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'gonzalez.views.index', name='index'),
+    url(r'^clientes/$', 'gonzalez.clientes.views.lista_clientes', name='lista_cleintes'),
     # url(r'^gonzalez/', include('gonzalez.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += patterns('django.views',(r'^media/(?P<path>.*)$', 'static.serve', {'document_root': MEDIA_ROOT}))
